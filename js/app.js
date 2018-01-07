239c1d98301737690378147b665869dfcb860e06
jessia2011 <jessicamezarojas2011@gmail.com>
2018-01-07 14:21:50 -0500

    añadiendo maquetado del perfil de usurio

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+$ (document).ready(function(){
+  $(".button-collapse").sideNav();
+
+
+  var $btnPublicar = $('#btn-publicar');
+  var $txArea = $('#textarea1');
+
+  /* Activar boton publicar */
+  $txArea.keyup(function() {
+    var longTxArea = $txArea.val().length;
+    longTxArea >= 1 ? activeButton () : desactiveButton();
+  });
+
+
+
+
+
+  // Aqui indicar que se puede implementar la función como variable
+  function activeButton () {
+    $btnPublicar.removeClass('disabled');  
+  }
+  function desactiveButton() {
+    $btnPublicar.addClass('disabled');
+  } 
+
+});
