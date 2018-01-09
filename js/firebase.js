79efaaccc3ce47dbee73d3113c4cca14973e846e
jessia2011 <jessicamezarojas2011@gmail.com>
2018-01-09 11:51:17 -0500

    logiando on fc

--- js/firebase.js
+++ js/firebase.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
       .catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(errorCode);
+        alert(errorCode);
         alert(errorMessage);
       });
   });
@@ -31,8 +31,32 @@ $(document).ready(function() {
       .catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(errorCode);
+        alert(errorCode);
         alert(errorMessage);
       });
+  });
+
+  $('#btn-face').click(function() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
   });
 });
